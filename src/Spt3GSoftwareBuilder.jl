59576a6ae6678ecc8283d903268c91898c5ae3e0
marius <mariusmillea@gmail.com>
2023-01-29 15:31:41 -0800

    tweak boost vars

--- src/Spt3GSoftwareBuilder.jl
+++ src/Spt3GSoftwareBuilder.jl
@@ -14,31 +14,33 @@ function cmake_flags_dict()
 
     Dict(
 
-        :Python_EXECUTABLE    => Python_jll.python_path,
+        :Python_EXECUTABLE     => Python_jll.python_path,
 
-        :Boost_INCLUDE_DIR    => joinpath(boost_jll.artifact_dir,  "include"),
-        :Boost_LIBRARY_DIR    => joinpath(boost_jll.artifact_dir,  "lib"),
+        :BOOST_ROOT            => boost_jll.artifact_dir,        
+        :BOOST_INCLUDEDIR      => joinpath(boost_jll.artifact_dir,  "include"),
+        :BOOST_LIBRARYDIR      => joinpath(boost_jll.artifact_dir,  "lib"),
+        :Boost_NO_SYSTEM_PATHS => "ON",
         
-        :GSL_INCLUDES         => joinpath(GSL_jll.artifact_dir,    "include"),
-        :GSL_LIB              => joinpath(GSL_jll.artifact_dir,    "lib/libgsl.so"),
-        :GSL_CBLAS_LIB        => joinpath(GSL_jll.artifact_dir,    "lib/libgslcblas.so"),
+        :GSL_INCLUDES          => joinpath(GSL_jll.artifact_dir,    "include"),
+        :GSL_LIB               => joinpath(GSL_jll.artifact_dir,    "lib/libgsl.so"),
+        :GSL_CBLAS_LIB         => joinpath(GSL_jll.artifact_dir,    "lib/libgslcblas.so"),
         
-        :FFTW_INCLUDES        => joinpath(FFTW_jll.artifact_dir,   "include"),
-        :FFTW_LIBRARIES       => joinpath(FFTW_jll.artifact_dir,   "lib/libfftw3.so"),
-        :FFTW_THREADS_LIBRARY => joinpath(FFTW_jll.artifact_dir,   "lib/libfftw3.so"),
+        :FFTW_INCLUDES         => joinpath(FFTW_jll.artifact_dir,   "include"),
+        :FFTW_LIBRARIES        => joinpath(FFTW_jll.artifact_dir,   "lib/libfftw3.so"),
+        :FFTW_THREADS_LIBRARY  => joinpath(FFTW_jll.artifact_dir,   "lib/libfftw3.so"),
         
-        :NETCDF_INCLUDES      => joinpath(NetCDF_jll.artifact_dir, "include"),
-        :NETCDF_LIBRARIES     => joinpath(NetCDF_jll.artifact_dir, "lib/libnetcdf.so"),
+        :NETCDF_INCLUDES       => joinpath(NetCDF_jll.artifact_dir, "include"),
+        :NETCDF_LIBRARIES      => joinpath(NetCDF_jll.artifact_dir, "lib/libnetcdf.so"),
         
-        :HDF5_INCLUDES        => joinpath(HDF5_jll.artifact_dir,   "include"),
-        :HDF5_LIBRARIES       => joinpath(HDF5_jll.artifact_dir,   "lib/libhdf5.so"),
-        :HDF5_HL_LIBRARIES    => joinpath(HDF5_jll.artifact_dir,   "lib/libhdf5_hl.so"),
+        :HDF5_INCLUDES         => joinpath(HDF5_jll.artifact_dir,   "include"),
+        :HDF5_LIBRARIES        => joinpath(HDF5_jll.artifact_dir,   "lib/libhdf5.so"),
+        :HDF5_HL_LIBRARIES     => joinpath(HDF5_jll.artifact_dir,   "lib/libhdf5_hl.so"),
 
-        :FLAC_INCLUDE_DIR     => joinpath(FLAC_jll.artifact_dir,   "include"),
-        :FLAC_LIBRARIES       => joinpath(FLAC_jll.artifact_dir,   "lib/libFLAC.so"),
+        :FLAC_INCLUDE_DIR      => joinpath(FLAC_jll.artifact_dir,   "include"),
+        :FLAC_LIBRARIES        => joinpath(FLAC_jll.artifact_dir,   "lib/libFLAC.so"),
 
         :CMAKE_BUILD_WITH_INSTALL_RPATH => "TRUE",
-        :CMAKE_INSTALL_RPATH => "'$(join(Set(vcat(
+        :CMAKE_INSTALL_RPATH => "'\$ORIGIN:$(join(Set(vcat(
             boost_jll.LIBPATH_list, 
             GSL_jll.LIBPATH_list, 
             FFTW_jll.LIBPATH_list, 
